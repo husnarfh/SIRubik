cb82fb9b86765a09ababd9c90e8868902bce805d
a <alfakatsuki@gmail.com>
2018-11-08 14:51:07 +0700

    add admin lte

--- backend/sirubik/database/migrations/2018_11_07_022950_create_table.php
+++ backend/sirubik/database/migrations/2018_11_07_022950_create_table.php
@@ -13,10 +13,59 @@ class CreateTable extends Migration
      */
     public function up()
     {
-        Schema::create('pages', function (Blueprint $table) {
-            $table->increments('id');
+
+        Schema::create('relawan', function (Blueprint $table) {
+            $table->increments('id_relawan');
+            $table->string('email');
+            $table->string('password');
+            $table->string('nama_lengkap');
+            $table->string('alamat');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->date('waktu_masuk');
+            $table->string('no_hp');
+            $table->string('id_line');
+            $table->string('role');
+        });
+
+        Schema::create('pengurus', function (Blueprint $table) {
+            $table->increments('id_pengurus');
+            $table->string('email');
+            $table->string('password');
+            $table->string('nama_lengkap');
+            $table->string('alamat');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->date('waktu_masuk');
+            $table->string('no_hp');
+            $table->string('id_line');
+            $table->string('role');
+        });
+
+
+        Schema::create('lamaran', function (Blueprint $table) {
+            $table->increments('id_lamaran');
+            $table->timestamps();
+            
+        });
+
+        Schema::create('jadwal', function (Blueprint $table) {
+            $table->increments('id_jadwal');
+            $table->string('id_mata_pelajaran');
+            $table->time('waktu');
             $table->timestamps();
         });
+
+        Schema::create('mata_pelajaran', function (Blueprint $table) {
+            $table->increments('id_mata_pelajaran');
+            $table->timestamps();
+        });
+        
+        Schema::create('materi', function (Blueprint $table) {
+            $table->increments('id_materi');
+            $table->timestamps();
+        });
+        
     }
 
     /**
