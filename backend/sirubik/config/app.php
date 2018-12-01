b3156b75dab42ef6a34ec3cbd5ecf69d46e46004
a <alfakatsuki@gmail.com>
2018-12-01 19:23:31 +0700

    naon

--- backend/sirubik/config/app.php
+++ backend/sirubik/config/app.php
@@ -160,7 +160,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        // Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         // Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
     ],
@@ -211,8 +211,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'JWTAuth'   => Tymon\JWTAuthFacades\JWTAuth::class,
-        'JWTFactory' => Tymon\JWTAuthFacades\JWTFactory::class,
+        // 'JWTAuth'   => Tymon\JWTAuthFacades\JWTAuth::class,
+        // 'JWTFactory' => Tymon\JWTAuthFacades\JWTFactory::class,
     ],
 
 ];
