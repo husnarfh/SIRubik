d32e424605d17ea52832251e83910de71fa8e720
DarkBoyOKI <okimonoki@rocketmail.com>
2018-12-19 19:16:25 +0700

    trashes

--- frontend_web (Error)/resources/views/templates/index/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 5.5.5
-    </div>
-    <strong>Copywrong &copy; 2018 KELOMPOK RUBICK.</strong> All wrongs
-    reserved.
-  </footer>
